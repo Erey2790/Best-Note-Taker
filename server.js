90df4fc60a3cd8556f0d985215c6c65c51436903
ElmerPC <erey2790@gmail.com>
2022-02-28 20:45:09 -0600

    corrected and went back to old way

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 // Import express package
 const express = require('express');
-// Import Routes
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
+const path = require('path');
 
 
 // Initialize our app variable by setting it to the value of express()
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    // `res.sendFile` is Express' way of sending a file
+    // `__dirname` is a variable that always returns the directory that your server is running in
+    res.sendFile(path.join(__dirname + '/index.html'));
+  });
+
+app.get('/notes', (req, res) => {
+    // `res.sendFile` is Express' way of sending a file
+  // `__dirname` is a variable that always returns the directory that your server is running in
+  res.sendFile(path.join(__dirname + '/public/notes.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
